f6e177ec96b78aa970088dd08091137d00b92452
yyd3157 <yyd3157@naver.com>
2023-10-27 21:52:06 +0900

    refactor: 새로운 유저 정보 firebase에 저장 될 때 해당 정보 유저 정보 요청 하도록 구현

--- app/src/main/java/com/young/sportsmatch/ui/setting/SettingViewModel.kt
+++ app/src/main/java/com/young/sportsmatch/ui/setting/SettingViewModel.kt
@@ -1,10 +1,14 @@
 package com.young.sportsmatch.ui.setting
 
 import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.young.sportsmatch.data.model.User
 import com.young.sportsmatch.data.source.SettingRepository
 import kotlinx.coroutines.launch
 
@@ -14,10 +18,28 @@ class SettingViewModel(
 ) : ViewModel() {
 
     private var imageUrl: Uri? = null
+    private val _userInfo = MutableLiveData<User?>()
+    val userInfo: LiveData<User?> = _userInfo
 
     fun addUser(nickname: String) {
         viewModelScope.launch {
-            repository.addUser(nickname, imageUrl.toString())
+            val response = repository.addUser(nickname, imageUrl.toString())
+            if (response.isSuccessful) {
+                getUser()
+            }
+        }
+    }
+
+    fun getUser() {
+        viewModelScope.launch {
+            val response = repository.getUser()
+            Log.d("post", "$response")
+            if (response.isSuccessful) {
+                val user = response.body()
+                _userInfo.postValue(user)
+            } else {
+                _userInfo.postValue(null)
+            }
         }
     }
 
