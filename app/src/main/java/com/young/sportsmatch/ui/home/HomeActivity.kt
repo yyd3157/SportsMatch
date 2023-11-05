011bc73661591e7dfd98908294ce53322850c74f
yyd3157 <yyd3157@naver.com>
2023-11-06 00:16:11 +0900

    refactor: FloatingActionButton 기능 구현

--- app/src/main/java/com/young/sportsmatch/ui/home/HomeActivity.kt
+++ app/src/main/java/com/young/sportsmatch/ui/home/HomeActivity.kt
@@ -3,12 +3,16 @@ package com.young.sportsmatch.ui.home
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.young.sportsmatch.R
 import com.young.sportsmatch.databinding.ActivityHomeBinding
 import com.young.sportsmatch.ui.setting.SettingActivity
+import com.young.sportsmatch.ui.write.WriteFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
@@ -22,6 +26,10 @@ class HomeActivity : AppCompatActivity() {
             moveToSetting()
         }
 
+        binding.writeButton.setOnClickListener {
+            navigateToWrite()
+        }
+
         val navController =
             supportFragmentManager.findFragmentById(R.id.home_container)?.findNavController()
         navController?.let {
@@ -31,5 +39,15 @@ class HomeActivity : AppCompatActivity() {
 
     private fun moveToSetting() {
         startActivity(Intent(this, SettingActivity::class.java))
+    }
+
+    private fun navigateToWrite() {
+        replaceFragment(WriteFragment())
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.home_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
