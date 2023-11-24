362f8d4252974961189b7c5e0f3534a9c8698be0
yyd3157 <yyd3157@naver.com>
2023-11-24 17:09:26 +0900

    feat: 데이터 베이스 생성

---
+++ app/src/main/java/com/young/sportsmatch/database/Converter.kt
@@ -0,0 +1,19 @@
+package com.young.sportsmatch.database
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.young.sportsmatch.data.model.Post
+import dagger.hilt.android.scopes.ActivityScoped
+
+@ActivityScoped
+class Converter() {
+    @TypeConverter
+    fun postToString(post: Post?): String? {
+        return Gson().toJson(post)
+    }
+
+    @TypeConverter
+    fun stringToPost(postJson: String?): Post? {
+        return Gson().fromJson(postJson, Post::class.java)
+    }
+}
