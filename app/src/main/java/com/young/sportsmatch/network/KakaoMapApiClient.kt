7c79bfe540a174ee3da381ab6e050b1d3d7e44e8
yyd3157 <yyd3157@naver.com>
2023-12-05 23:33:48 +0900

    refactor: 검색 결과 값 10개로 수정

--- app/src/main/java/com/young/sportsmatch/network/KakaoMapApiClient.kt
+++ app/src/main/java/com/young/sportsmatch/network/KakaoMapApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface KakaoMapApiClient {
-    @GET("v2/local/search/keyword.json?page=3&size=3")
+    @GET("v2/local/search/keyword.json?page=1&size=10")
     suspend fun getSearchAddress(
         @Header("Authorization") key: String,
         @Query("query") query: String,
